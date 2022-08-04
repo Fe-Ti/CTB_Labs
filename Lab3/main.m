9c450803c3fb9081352ce585247202d4383d751c
Fёdor T <btm.007@mail.ru>
2022-08-04 21:55:15 +0300

    Fix: #-style comment

--- Lab3/main.m
+++ Lab3/main.m
@@ -26,8 +26,6 @@ sys = w1 * w2 * w3 * w4
 % from 0.1*2pi to 10*2pi with 5000 points
 w = 2*pi*logspace(-2, 2, 5*10^3);
 
-#[0 0.1:0.01:10, 20:1:100, 200:100:1000];
-
 figure('Name', 'nyquist(sys)');
 nyquist(sys,w);
 draw_circle(1);
